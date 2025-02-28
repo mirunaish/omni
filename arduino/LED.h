5f8572f9e21250a6fd86bf3da2d3b4d2adc5a436
miruna <58366207+mirunaish@users.noreply.github.com>
2025-02-28 07:08:38 -0500

    added blush

--- arduino/LED.h
+++ arduino/LED.h
@@ -6,6 +6,7 @@ class LED {
   private:
     int pin;
     int brightness = 0;
+    int blushTimer = 0;
 
   public:
     LED() {}
@@ -16,11 +17,23 @@ class LED {
       pinMode(pin, OUTPUT);
     }
 
+    void setBrightness(int value) {
+      this->brightness = max(0, min(value, 255));  // clamp to [0, 255]
+      analogWrite(pin, brightness);
+    }
+
     void loop() {
-      ;
+      if (blushTimer > 0) {
+        blushTimer--;
+        // fade in blush
+        if (brightness < 255) setBrightness(brightness + Consts::blushSpeed);
+      } else if (blushTimer == 0 && brightness > 0) {
+        // fade out blush
+        setBrightness(brightness - Consts::blushSpeed);
+      }
     }
 
     void blush() {
-      ; 
+      blushTimer = getFrames(Consts::blushDuration);
     }
 };
--- arduino/consts.h
+++ arduino/consts.h
@@ -11,8 +11,8 @@ class Consts {
     static const int patThreshold = 300;  // minimum force sensor value that is registered as a headpat
 
     // cheek LED
-    static const int fadeSpeed = 5;
-    static const int fadeDuration = 5;  // in seconds
+    static const int blushSpeed = 5;
+    static const int blushDuration = 5;  // in seconds
 
     // arm
     static const int neutralAngle = 90;    
