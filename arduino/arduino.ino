801c8963fa2cac093662c97c94e30cc52dd3a30c
miruna <58366207+mirunaish@users.noreply.github.com>
2025-02-28 00:29:34 -0500

    moving stuff

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -2,17 +2,18 @@
 #include <SPI.h>
 #include <TFT_eSPI.h>
 
-// face 
-int led_R = 3;
-int led_L = 3;
-int fadeAmount = 5;
+/* ------- initialize inputs + outputs ------- */
 
-// connecting the screen 
+// screen
 #define TFT_CS   10  // Chip Select pin
 #define TFT_DC   9   // Data/Command pin
 #define TFT_RST  8   // Reset pin
 TFT_eSPI tft = TFT_eSPI();
 
+// face 
+int led_R = 3;
+int led_L = 3;
+int fadeAmount = 5;
 
 // arms 
 Servo servo_R;
@@ -26,15 +27,14 @@ int neutral_degree = 90;
 int current_L = 90; 
 int current_R = 90; 
 
-// torso sound 
-
+// torso sound
+// TODO
 
 void setup() {
   // face 
   pinMode(led_R, OUTPUT);
   
-  // screen 
-  Serial.begin(115200);
+  // screen
   tft.init();
   tft.setRotation(0);
   tft.fillScreen(TFT_BLACK);
@@ -42,7 +42,8 @@ void setup() {
   // arms 
   servo_R.attach(5); 
   servo_L.attach(6); 
-  // Serial.begin(9600);
+
+  Serial.begin(115200);
 }
 
 void one_hand_wave(int times = 2){
@@ -64,7 +65,7 @@ void one_hand_wave(int times = 2){
 }
 
 void two_hand_wave(int times = 2){
-// wave both hands up and down x times
+  // wave both hands up and down x times
   for (int t = 0; t <= times; t+=1){
     for (int out_degrees=0; out_degrees <= 20 ; out_degrees +=2){
       current_R = current_R + out_degrees; 
@@ -101,12 +102,9 @@ void blush(){
     analogWrite(led_L, led_value); 
     delay(15); 
   }
-
 }
 
-
 void loop() {
-
   // set initial values for servo
   servo_R.write(neutral_degree); 
   delay(15); 
@@ -120,24 +118,24 @@ void loop() {
   // test blushing 
   // blush(); 
 
-// draw face 
-// eyes 
-tft.fillRect(60, 60, 30, 30, TFT_GREEN);
-tft.fillRect(150,60 ,30, 30, TFT_GREEN); 
+  // draw face 
+  // eyes 
+  tft.fillRect(60, 60, 30, 30, TFT_GREEN);
+  tft.fillRect(150,60 ,30, 30, TFT_GREEN); 
 
-// meh mouth 
-tft.fillRect(20, 150, 200, 30, TFT_GREEN); 
+  // meh mouth 
+  tft.fillRect(20, 150, 200, 30, TFT_GREEN); 
 
-// tongue out 
-tft.fillRect(150, 180, 40, 40, TFT_RED); 
+  // tongue out 
+  tft.fillRect(150, 180, 40, 40, TFT_RED); 
 
   // // write the position outputs of the pots
   // val_R = analogRead(pot_R);
-  // val_L = analogRead(pot_L); 
+  // val_L = analogRead(pot_L);
 
-  // Serial.print(val_R);
-  // Serial.print(",");
-  // Serial.println(val_L); 
+  Serial.print(val_R);
+  Serial.print(",");
+  Serial.println(val_L); 
 
-  delay(3*1000); 
+  delay(3*1000);
 }
