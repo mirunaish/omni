7d0c5b562fe64cc531c139783a3434da1a4c0cdc
miruna <58366207+mirunaish@users.noreply.github.com>
2025-03-04 17:34:32 -0500

    big arduino

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -2,14 +2,15 @@
 #include "utils.h"
 #include "serial.h"
 #include "screen.h"
-// #include "arm.h"
-// #include "LED.h"
-// #include "forceSensor.h"
+#include "arm.h"
+#include "LED.h"
+#include "forceSensor.h"
 
-// ForceSensor head;  // force sensor
-// LED cheeks;  // LEDs
-// Arm leftArm;
-// Arm rightArm;
+ForceSensor head;  // force sensor
+LED cheeks;  // LEDs
+Screen screen;  // face
+Arm leftArm;
+Arm rightArm;
 // TODO add speaker
 
 int frames = 0; // for testing, will be removed
@@ -22,17 +23,17 @@ void setup() {
 
   // set up all the parts of the robot
 
-  // head = ForceSensor(A2);
-  // cheeks = LED(3);
+  head = ForceSensor(A4);
+  cheeks = LED(4);
 
-  // leftArm = Arm(A0, 6, "LEFT");
-  // rightArm = Arm(A1, 5, "RIGHT");
+  leftArm = Arm(A0, 2, "LEFT");
+  rightArm = Arm(A2, 3, "RIGHT");
 
-  screenSetup();
+  screen = Screen();
 
   // calibrate arms
-  // leftArm.calibrate();
-  // rightArm.calibrate();
+  leftArm.calibrate();
+  rightArm.calibrate();
 
   frames = 0;
 }
@@ -50,51 +51,54 @@ void loop() {
     
     Message message = readMessageFromSerial();
 
-    // if (message.type == "HEADPAT") {
-    //   cheeks.blush();
-    // } else if (message.type == "WAVE") {
-    //   String words[MAX_WORDS];
-    //   splitIntoWords(message.payload, words);
-    //   String name = words[0];
-    //   int value = words[1].toInt();
-    //   Serial.println("LOG parsed value from string " + String(value));  // tell client what value i parsed, to make sure it's correct
+    if (message.type == "HEADPAT") {
+      cheeks.blush();
+    }
 
-    //   if (name == "LEFT") leftArm.moveTo(value);
-    //   else if (name == "RIGHT") rightArm.moveTo(value);
-    //   else Serial.println("ERROR unknown arm " + name);
-    // }
-    // if (message.type == "PIXEL_SCALED") {
-    //   int numberCount = 0;
-    //   parseNumbers(message.payload, numbers, &numberCount);
-    //   Serial.println("LOG received " + String(numberCount) + " numbers");  // tell client i got the message
-    //   for (int i=0; i<numberCount/3; i++) {
-    //     screenSetPixel(numbers[i*3], numbers[i*3+1], numbers[i*3+2]);
-    //   }
-    //   return;  // skip delay
-    // }
-    // else
-    if (message.type == "PIXEL") {
+    else if (message.type == "WAVE") {
+      String words[MAX_WORDS];
+      splitIntoWords(message.payload, words);
+      String name = words[0];
+      int value = words[1].toInt();
+      Serial.println("LOG parsed value from string " + String(value));  // tell client what value i parsed, to make sure it's correct
+
+      if (name == "LEFT") leftArm.moveTo(value);
+      else if (name == "RIGHT") rightArm.moveTo(value);
+      else Serial.println("ERROR unknown arm " + name);
+    }
+
+    else if (message.type == "PIXEL_SCALED") {
       int numberCount = 0;
       parseNumbers(message.payload, numbers, &numberCount);
       Serial.println("LOG received " + String(numberCount) + " numbers");  // tell client i got the message
-      screenSetPixels(numbers[0], numbers[1], numbers[2], &numbers[3]);
+      for (int i=0; i<numberCount/3; i++) {
+        screen.setPixel(numbers[i*3], numbers[i*3+1], numbers[i*3+2]);
+      }
+      return;  // skip delay
+    }
+
+    else if (message.type == "PIXEL") {
+      int numberCount = 0;
+      parseNumbers(message.payload, numbers, &numberCount);
+      Serial.println("LOG received " + String(numberCount) + " numbers");  // tell client i got the message
+      screen.setPixels(numbers[0], numbers[1], numbers[2], &numbers[3]);
     
       return;  // skip delay
     }
 
-    // else {
-    //   Serial.println("ERROR unknown message type " + message.type);
-    // }
+    else {
+      Serial.println("ERROR unknown message type " + message.type);
+    }
   }
 
   // tell sensors to listen for changes and outputs to update their values
   
-  // head.loop();
-  // cheeks.loop();
-  // leftArm.loop();
-  // rightArm.loop();
+  head.loop();
+  cheeks.loop();
+  leftArm.loop();
+  rightArm.loop();
 
-  // screenLoop();
+  screen.loop();
 
   Serial.flush();  // force serial to write data
   delay(FRAME_DELAY);
--- arduino/screen.h
+++ arduino/screen.h
@@ -7,34 +7,39 @@
 #define TFT_DC 9  // Data/Command pin
 #define TFT_RST 8  // Reset pin
 
-TFT_eSPI tft = TFT_eSPI();
-int cursorY = 50;
 
-screenSetup() {
-  tft.init();
-  tft.setRotation(2);  // TODO change back to 0?
-  tft.fillScreen(TFT_BLACK);  // initialize to black
-}
+class Screen {
+  private:
+    TFT_eSPI tft = TFT_eSPI();
+    int cursorY = 50;
 
-void screenPrint(String text) {
-  tft.setTextColor(TFT_WHITE);
-  tft.setTextSize(1);
-  tft.setCursor(20, cursorY);
-  tft.println(text);
-  cursorY+=10;
-}
+  public:
+    Screen() {
+      tft.init();
+      tft.setRotation(2);  // TODO change back to 0?
+      tft.fillScreen(TFT_BLACK);  // initialize to black
+    }
 
-void screenSetPixel(int x, int y, int color) {
-  if (SCREEN_SCALE == 1) tft.drawPixel(x, y, color);
-  else tft.fillRect(x*SCREEN_SCALE, y*SCREEN_SCALE, SCREEN_SCALE, SCREEN_SCALE, color);
-}
+    void print(String text) {
+      tft.setTextColor(TFT_WHITE);
+      tft.setTextSize(1);
+      tft.setCursor(20, cursorY);
+      tft.println(text);
+      cursorY+=10;
+    }
 
-void screenSetPixels(int x, int y, int size, int colors[MAX_PIXELS]) {
-  // set colors of pixels in the size*size rectangle with corner at x, y
-  tft.setAddrWindow(x, y, size, size);
-  tft.pushColors(colors, size*size);
-}
+    void setPixel(int x, int y, int color) {
+      if (SCREEN_SCALE == 1) tft.drawPixel(x, y, color);
+      else tft.fillRect(x*SCREEN_SCALE, y*SCREEN_SCALE, SCREEN_SCALE, SCREEN_SCALE, color);
+    }
 
-void screenLoop() {
-  // in case anything needs to be updated every frame
-}
+    void setPixels(int x, int y, int size, int colors[MAX_PIXELS]) {
+      // set colors of pixels in the size*size rectangle with corner at x, y
+      tft.setAddrWindow(x, y, size, size);
+      tft.pushColors(colors, size*size);
+    }
+
+    void loop() {
+      // in case anything needs to be updated every frame
+    }
+};
